999b6368c18222315e5a17a4ef922a8321bf2457
velmurugan444 <mvel1620r@gmail.com>
2024-01-12 13:55:34 +0530

    changed url in backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://ecommercebackend-lvj0.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
